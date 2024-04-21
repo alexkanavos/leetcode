5523802ecb2a8d41d3a70d9a58afeb83c7da913e
Alex Kanavos <alexkanv@gmail.com>
2024-04-21 10:36:17 +0200

    Time: 36 ms (83.54%), Space: 16.7 MB (16.83%) - LeetHub

---
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        answer: list[str] = []
+        for word in words:
+            for el in words:
+                if word != el:
+                    if el in word:
+                        answer.append(el)
+        answer = set(answer)
+        return list(answer)
